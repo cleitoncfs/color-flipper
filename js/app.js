ccd68cf963f233382a1f966a4fdd3441eb53ab86
cleitoncfs <cleitoncfs@gmail.com>
2025-12-11 18:43:45 +0000

    Update

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.span-color');
+const mainColor = document.getElementById('main-color');
 
 btn.addEventListener('click', function () {
   const randomNumber = getRandomNumbers();
-  document.body.style.backgroundColor = colors[randomNumber];
+  mainColor.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
